bf160331a474e55b11c17054ac73532a96b06453
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-07-23 10:53:13 +0900

    0723 10:53

---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace Animal_0723
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Animalクラスであるa1を用意し、実体化（インスタンス化）
+            Animal a1 = new Animal();
+
+            a1.DataChange("犬", 5);
+
+        }
+    }
+}
