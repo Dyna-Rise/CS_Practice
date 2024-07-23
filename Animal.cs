613486ae060aa9fbe339405093d7dd9f9e6f1ca0
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-07-23 10:31:19 +0900

    0723 10：30

---
+++ Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_0723
+{
+    internal class Animal
+    {
+        //クラスが持っている変数（フィールド）
+        private string name; //動物の名称
+        private string genre; //分類
+        private int age; //年齢
+        private string color; //見た目の色
+        private string special; //特徴・特技
+        private string cry; //鳴き声
+
+        //機能（メソッド（関数））
+        //フィールドのデータを変更するメソッド
+        public void DataChange(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
